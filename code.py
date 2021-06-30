d9d7fb6abfda1ccb6946e928f420552fde583d48
Ankan Bera <eklapothik54@gmail.com>
2021-07-01 01:21:15 +0530

    Update code.py
    changed meta charset to fix bugs regarding special characters not coming correctly

--- code.py
+++ code.py
@@ -16,7 +16,7 @@ def write_to_html(title, headlines, summaries, links, min_len, base_url):
                 <!DOCTYPE html>
                 <html lang="en">
                 <head>
-                    <meta charset="UTF-8">
+                    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <title>''' +title.text +'''</title>
